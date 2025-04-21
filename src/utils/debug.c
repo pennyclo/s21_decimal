399c3a50844527bff1f01f99c2f68ac76fcfcd2c
pennyclo <alexandr_sav@outlook.com>
2025-04-21 08:55:41 +0500

    chore(debug): unwork case

--- src/utils/debug.c
+++ src/utils/debug.c
@@ -50,8 +50,8 @@ void print_flex(flex_int x) {
 }
 
 int main() {
-  s21_decimal x = {{255, 0, 0, 0}};
-  s21_decimal y = {{3, 0, 0, 0}};
+  s21_decimal x = {{4294967295u, 4294967295u, 4294967295u, 0}};
+  s21_decimal y = {{4294967294u, 4294967294u, 4294967294u, 0}};
   s21_decimal z = {{252, 0, 0, 0}};
 
   flex_int x1 = decimal_to_flex(x);
