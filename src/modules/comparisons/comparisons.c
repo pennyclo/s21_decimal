9c85b412be63812e8d5b4bd662ab29017016367c
pennyclo <alexandr_sav@outlook.com>
2025-07-29 13:59:25 +0500

    feat(decimal): add func is_equal & fix is_greater_or_equal

--- src/modules/comparisons/comparisons.c
+++ src/modules/comparisons/comparisons.c
@@ -121,7 +121,8 @@ int s21_is_greater_or_equal(s21_decimal *dec1, s21_decimal *dec2) {
   if (decimal_1.data_size > decimal_2.data_size || eq_signs == 1) {
     is_greater = 1;
   } else if (decimal_1.data_size == decimal_2.data_size && eq_signs == 0) {
-    for (int i = decimal_1.data_size - 1; i >= 0 && !is_greater; i--) {
+    is_greater = 1;
+    for (int i = decimal_1.data_size - 1; i >= 0 && is_greater; i--) {
       uint8_t check1 = CHECK_DEC_BIT(decimal_1.data, i, decimal_1.data_size);
       uint8_t check2 = CHECK_DEC_BIT(decimal_2.data, i, decimal_2.data_size);
 
@@ -138,4 +139,35 @@ int s21_is_greater_or_equal(s21_decimal *dec1, s21_decimal *dec2) {
   }
 
   return is_greater;
+}
+
+int s21_is_equal(s21_decimal *dec1, s21_decimal *dec2) {
+  int is_equal = 0;
+
+  flex_int decimal_1 = decimal_to_flex(dec1);
+  flex_int decimal_2 = decimal_to_flex(dec2);
+
+  int eq_signs = eq_sign(&decimal_1.service, &decimal_2.service);
+
+  normalization(&decimal_1, &decimal_2);
+
+  if (decimal_1.data_size == decimal_2.data_size && eq_signs == 0) {
+    is_equal = 1;
+    for (int i = decimal_1.data_size - 1; i >= 0 && is_equal; i--) {
+      uint8_t check1 = CHECK_DEC_BIT(decimal_1.data, i, decimal_1.data_size);
+      uint8_t check2 = CHECK_DEC_BIT(decimal_2.data, i, decimal_2.data_size);
+
+      is_equal = (check1 == check2) ? 1 : 0;
+    }
+  }
+
+  if (decimal_1.data) {
+    free(decimal_1.data);
+  }
+
+  if (decimal_2.data) {
+    free(decimal_2.data);
+  }
+
+  return is_equal;
 }
