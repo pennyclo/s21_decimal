5eb41daaf57fc0f193f4f577363998ccfe47008a
pennyclo <alexandr_sav@outlook.com>
2025-07-29 14:05:07 +0500

    feat(decimal): add func is_not_equal

--- src/modules/comparisons/comparisons.c
+++ src/modules/comparisons/comparisons.c
@@ -171,3 +171,34 @@ int s21_is_equal(s21_decimal *dec1, s21_decimal *dec2) {
 
   return is_equal;
 }
+
+int s21_is_not_equal(s21_decimal *dec1, s21_decimal *dec2) {
+  int is_not_equal = 1;
+
+  flex_int decimal_1 = decimal_to_flex(dec1);
+  flex_int decimal_2 = decimal_to_flex(dec2);
+
+  int eq_signs = eq_sign(&decimal_1.service, &decimal_2.service);
+
+  normalization(&decimal_1, &decimal_2);
+
+  if (decimal_1.data_size == decimal_2.data_size && eq_signs == 0) {
+    is_not_equal = 0;
+    for (int i = decimal_1.data_size - 1; i >= 0 && !is_not_equal; i--) {
+      uint8_t check1 = CHECK_DEC_BIT(decimal_1.data, i, decimal_1.data_size);
+      uint8_t check2 = CHECK_DEC_BIT(decimal_2.data, i, decimal_2.data_size);
+
+      is_not_equal = (check1 == check2) ? 0 : 1;
+    }
+  }
+
+  if (decimal_1.data) {
+    free(decimal_1.data);
+  }
+
+  if (decimal_2.data) {
+    free(decimal_2.data);
+  }
+
+  return is_not_equal;
+}
